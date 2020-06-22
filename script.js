3c43b0f8c057cd9dc88f569631ebe1f034170d02
Alex Shmidt <kav993@gmail.com>
2020-06-22 16:55:27 +0200

    add footer and contacts sections

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function navCheckbox(){
 		navBar.style.removeProperty('margin-bottom')
 	}
 }
+
+
+const collapsibles = document.getElementsByClassName('collapsible');
+
+for(const collapsible of collapsibles){
+	const button = collapsible.children[0];
+	button.addEventListener('click', (ev) => {
+		const content = collapsible.children[1];
+		if(content.style.maxHeight){
+			content.style.maxHeight = null
+		} else {
+			content.style.maxHeight = content.scrollHeight + 200 + 'px'
+		}
+	})
+}
